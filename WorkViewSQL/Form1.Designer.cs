ae9c23ce332341ca4dfb614b88c9b1cf92b6c072
Rolling Vilches Tomasevich <rolling.vilches@antalis.com>
2019-06-05 09:59:58 -0400

    Initial commit

---
+++ WorkViewSQL/Form1.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace WorkViewSQL
+{
+    partial class frmWorkViewSQL
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmWorkViewSQL));
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.ofd = new System.Windows.Forms.OpenFileDialog();
+            this.btnImportData = new System.Windows.Forms.Button();
+            this.lblPath = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToOrderColumns = true;
+            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView1.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(1)))), ((int)(((byte)(51)))), ((int)(((byte)(42)))));
+            this.dataGridView1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(15, 180);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(922, 287);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.Visible = false;
+            // 
+            // ofd
+            // 
+            this.ofd.Filter = "Tddos los archivos|*.*";
+            // 
+            // btnImportData
+            // 
+            this.btnImportData.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnImportData.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.btnImportData.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnImportData.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnImportData.ForeColor = System.Drawing.Color.White;
+            this.btnImportData.Location = new System.Drawing.Point(806, 12);
+            this.btnImportData.Name = "btnImportData";
+            this.btnImportData.Size = new System.Drawing.Size(131, 23);
+            this.btnImportData.TabIndex = 4;
+            this.btnImportData.Text = "Import Data";
+            this.btnImportData.UseVisualStyleBackColor = false;
+            this.btnImportData.Click += new System.EventHandler(this.btnImportData_Click);
+            // 
+            // lblPath
+            // 
+            this.lblPath.AllowDrop = true;
+            this.lblPath.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblPath.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblPath.ForeColor = System.Drawing.Color.Lime;
+            this.lblPath.Location = new System.Drawing.Point(15, 14);
+            this.lblPath.Name = "lblPath";
+            this.lblPath.Size = new System.Drawing.Size(785, 19);
+            this.lblPath.TabIndex = 6;
+            this.lblPath.DragOver += new System.Windows.Forms.DragEventHandler(this.lblPath_DragOver);
+            this.lblPath.DoubleClick += new System.EventHandler(this.lblPath_DoubleClick);
+            // 
+            // frmWorkViewSQL
+            // 
+            this.AllowDrop = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightSlateGray;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(949, 574);
+            this.Controls.Add(this.lblPath);
+            this.Controls.Add(this.btnImportData);
+            this.Controls.Add(this.dataGridView1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "frmWorkViewSQL";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Work View SQL";
+            this.Load += new System.EventHandler(this.frmWorkViewSQL_Load);
+            this.DragOver += new System.Windows.Forms.DragEventHandler(this.frmWorkViewSQL_DragOver);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.OpenFileDialog ofd;
+        private System.Windows.Forms.Button btnImportData;
+        private System.Windows.Forms.Label lblPath;
+    }
+}
+
---
+++ WorkViewSQL/Form1.cs
@@ -0,0 +1,219 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.OleDb;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Excel = Microsoft.Office.Interop.Excel;
+using System.Threading;
+
+namespace WorkViewSQL
+{
+
+    public partial class frmWorkViewSQL : Form
+    {
+
+        System.Timers.Timer tmr = new System.Timers.Timer(1000);
+        delegate void myDeleg(Form toClose);
+        Form currentPrgBar = null;
+        Boolean createPrgBar = false;
+        Boolean disposePrgBar = false;
+
+        public frmWorkViewSQL()
+        {
+            InitializeComponent();
+        }
+
+        private void btnImportData_Click(object sender, EventArgs e)
+        {
+            dataGridView1.Visible = false;
+            Application.DoEvents();
+            this.Cursor = Cursors.WaitCursor;
+            dataGridView1.DataSource = null;
+            dataGridView1.Rows.Clear();
+            //dataGridView1.Refresh();
+            simulateLoad();
+            //Obtenemos el archivo desde la ubicación actual
+            var executableFolderPath = Path.GetDirectoryName(Assembly.GetEntryAssembly().Location);
+            //Hoja desde donde obtendremos los datos
+            string hoja = "Hoja1";
+            //Cadena de conexión
+            string conexion = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=" + //executableFolderPath +
+                            lblPath.Text +  //"C:\\paso\\SAP.xlsx" +
+                            ";Extended Properties='Excel 8.0;HDR=YES;';";
+
+            OleDbConnection con = new OleDbConnection(conexion);
+            //Consulta contra la hoja de Excel
+            OleDbCommand cmd = new OleDbCommand("Select * From [" + hoja + "$]", con);
+            try
+            {
+                //Conectarse al archivo de Excel
+                con.Open();
+
+                OleDbDataAdapter sda = new OleDbDataAdapter(cmd);
+                DataTable data = new DataTable();
+
+                //Cargar los datos
+                sda.Fill(data);
+
+                //Cargar la grilla
+                dataGridView1.DataSource = data;
+                //dataGridView1.AutoResizeColumns();
+                //dataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.AllCells;
+                dataGridView1.Visible = true;
+                
+            }
+            catch
+            {
+                //Error leyendo excel
+                MessageBox.Show("Ocurrió un error en la lectura del archivo");
+                dataGridView1.Visible = false;
+            }
+            finally
+            {
+                //Funcione o no, cerramos la cadena de conexión
+                con.Close();
+            }
+            this.Cursor = Cursors.Default;
+        }
+
+
+        public void simulateLoad()
+        {
+            createPrgBar = true;
+            //btnRunning.Text = "Load in progress";
+            for (int i = 0; i < 10000; i++)
+            {
+                //btnRunning.Text = "Running loop :" + i.ToString();
+                //btnRunning.Refresh();
+                Application.DoEvents();
+            }
+            //btnRunning.Text = "Run New Loop";
+            disposePrgBar = true;
+        }
+
+        /// watch if any process are running to create a new progress bar
+        void tmr_Elapsed(object sender, System.Timers.ElapsedEventArgs e)
+        {
+            if (createPrgBar & (currentPrgBar == null))
+            {
+                Thread myThread = new Thread(onFlyProgressBar);
+                myThread.Start();
+                createPrgBar = false;
+            }
+            else
+            {
+                if (disposePrgBar)
+                {
+                    closeForm(currentPrgBar);
+                    disposePrgBar = false;
+                }
+            }
+        }
+
+        /// Create new 'processing' progressBar
+        void onFlyProgressBar()
+        {
+            Label lblMessage = new Label
+            {
+                Dock = DockStyle.Top,
+                Text = "Procesing, please wait..",
+                TextAlign = System.Drawing.ContentAlignment.MiddleCenter,
+                Font = new System.Drawing.Font("Microsoft Sans Serif", 14,
+                                                System.Drawing.FontStyle.Bold,
+                                                System.Drawing.GraphicsUnit.Point)
+            };
+
+            ProgressBar prgBar = new ProgressBar
+            {
+                Height = 15,
+                Dock = DockStyle.Bottom,
+                Style = ProgressBarStyle.Marquee
+            };
+
+            currentPrgBar = new Form()
+            {
+                Width = 300,
+                Height = 50,
+                StartPosition = FormStartPosition.CenterScreen,
+                ControlBox = false,
+                FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D
+            };
+            currentPrgBar.Controls.AddRange(new Control[] { lblMessage, prgBar });
+            currentPrgBar.LostFocus += recoverFocus;
+            Application.Run(currentPrgBar);
+        }
+
+        void recoverFocus(object sender, EventArgs e)
+        {
+            currentPrgBar.TopMost = true;
+        }
+
+        /// Make current prgBar topmost if lost focus
+        void closeForm(Form toClose)
+        {
+            if (toClose == null) return;
+            if (toClose.InvokeRequired)
+            {
+                toClose.Invoke(new myDeleg(closeForm), toClose);
+            }
+            else
+            {
+                toClose.Close();
+                currentPrgBar = null;
+            }
+        }
+
+
+        private void frmWorkViewSQL_Load(object sender, EventArgs e)
+        {
+            tmr.Elapsed += tmr_Elapsed;
+            tmr.Interval = 100;
+            tmr.Start();
+
+            lblPath.BackColor = System.Drawing.Color.Transparent;
+            btnImportData.BackColor = System.Drawing.Color.Transparent;
+        }
+
+        private void lblPath_DoubleClick(object sender, EventArgs e)
+        {
+            ofd.Title = "Browse Text Files";
+            ofd.Filter = "Excel files (*.xl*)|*.xl*|Text files (*.txt;*.csv)|*.txt;*.csv|XML files (*.xml)|*.xml|All files (*.*)|*.*";
+            ofd.FilterIndex = 4;
+            ofd.CheckFileExists = true;
+            ofd.CheckPathExists = true;
+            ofd.RestoreDirectory = true;
+            ofd.InitialDirectory = @"C:\Station VY Canis Majoris\Proceso de Cierre\Clasificaciones ASTI_MAESTRO\Clasificacion Exp_SAP";  //Application.CommonAppDataPath; //@"C:\"
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                lblPath.Text = ofd.FileName;
+            }
+        }
+
+        private void lblPath_DragOver(object sender, DragEventArgs e)
+        {
+            string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
+            if (files != null && files.Length != 0)
+            {
+                lblPath.Text = files[0];
+            }
+        }
+
+        private void frmWorkViewSQL_DragOver(object sender, DragEventArgs e)
+        {
+            string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
+            if (files != null && files.Length != 0)
+            {
+                lblPath.Text = files[0];
+            }
+        }
+
+        
+    }
+}
